fbcb9b54b0dc0a48dd5007a7ca8ac5468accfb06
IgorK96 <44954943+IgorK96@users.noreply.github.com>
2019-03-13 01:00:37 +0100

    Update blinkWPi.c

--- V1/blinkWPi.c
+++ V1/blinkWPi.c
@@ -9,9 +9,9 @@ int main(){
 		pinMode(LED[i],OUTPUT);
 	while(1){
 		for(i=0;i<4;i++){
-		digitalWrite(LED[i],i);
+		digitalWrite(LED[i],1);
 		delay(200);
-		digitalWrite(LED[i],i);
+		digitalWrite(LED[i],0);
 		delay(200);
 		}
 	}
