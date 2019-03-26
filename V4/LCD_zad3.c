649aad8d4223c89dcfc6e29497683c2855292e9e
IgorK96 <44954943+IgorK96@users.noreply.github.com>
2019-03-26 01:04:41 +0100

    Update LCD_zad3.c

--- V4/LCD_zad3.c
+++ V4/LCD_zad3.c
@@ -1,122 +1,88 @@
+// gcc -o irm1 irm1.c -l bcm2835 -l lirc_client
+// sudo ./irm1
 
-#include <signal.h>
-#include <stdlib.h>
+//startuj lircs sudo /etc/init.d/lircd start
+
 #include <stdio.h>
-#include <wiringPi.h>
-#include <lcd.h>
+#include <stdlib.h>
 #include <string.h>
+#include <wiringPi.h>
+#include <lirc/lirc_client.h>
+#include <lcd.h>
 #include <time.h>
-char level0[8] = { 0b00000, 0b10101, 0b10101, 0b10101,
-                    0b10101, 0b10101, 0b11111, 0b00000};
-char level1[8] = { 0b11111, 0b01000, 0b01000, 0b01110,
-                    0b01001, 0b01001, 0b01001, 0b00000};
-char level2[8] = { 0b00000, 0b10010, 0b10010, 0b10010,
-                    0b10010, 0b10010, 0b10010, 0b00001};
+#include <errno.h>
 
-#define COLUMNS 16
-#define LCD_RS 3
-#define LCD_E 14
-#define LCD_D4 4
-#define LCD_D5 12
-#define LCD_D6 13
-#define LCD_D7 6
+// dodela vrednosti za konkretne pinove
+// prema gornjoj tabeli i semi DVK512
+const int RS = 3;
+const int EN = 14;
+const int D0 = 4;
+const int D1 = 12;
+const int D2 = 13;
+const int D3 = 6;
 
-void memory(void);
-void volume(void);
-void scrollText(void);
-
-char message[] = "Pozdravče!";
-int count =0;
-int j = 0;
-FILE *mem_file;
-char *temp;
-int lcd;
-
-int main(){
-  wiringPiSetup();
- 
-  if(lcd = lcdInit(2, 16, 4, LCD_RS, LCD_E, LCD_D4, LCD_D5, LCD_D6, LCD_D7, 0, 0, 0, 0)){
-    printf ("lcdInit nije uspeo! \n");
-    return -1 ;
-  }
-  
-  int uptimeTimer;
- 
-  while(1){
-    lcdClear (lcd);
-    volume();
-    sleep(1);
-    memory();
-    sleep(4);
-    lcdClear (lcd);
-    sleep(1);
-    scrollText();
-  }
-}
-void memory(void){
-  
-  char MemTotal[35];
-  char MemFree[35];
-  char total[35];
-  char free[35];
-
-    lcdClear (lcd);
-    mem_file=fopen("/proc/meminfo","r");
-    if(NULL != mem_file){
-      
-      fscanf(mem_file,"%*s%s%*s", MemTotal);
-      fscanf(mem_file,"%*s%s%*s", MemFree);
-      printf("\x1B[2J");//brisanje ekrana
-      lcdPosition(lcd,0,0);
-      lcdPrintf(lcd,"MemTotal-%sk",MemTotal);
-      lcdPosition(lcd,0,1);
-      lcdPrintf(lcd,"MemFree -%sk",MemFree);
-      fclose(mem_file);
-      }
-      else
-      {
-        printf("Otvaranje fajla \"/proc/meminfo\" nije uspelo!\n");
-      }
-}
-   void volume(void)
+int main()
 {
-  //Definicija specijalnih karaktera za Volume
-  lcdCharDef (lcd, 0, level0);
-  lcdCharDef (lcd, 1, level1);
-  lcdCharDef (lcd, 2, level2);
- 
-  lcdClear (lcd);
- 
-  int i;
-  lcdPosition (lcd, 9,1);
-  lcdPuts (lcd, ":Volume");
-  for (i = 0; i < 8; i++){
-    lcdPosition (lcd, i, 1);
-    lcdPutchar (lcd, i);
-    usleep(400000);
-  }
-}
-
-void scrollText(void)
-{
-  int i,n;
-  int h;
-  int tempSpace = 0;
-  char scrollPadding[] = " ";
-  int messageLength = strlen (scrollPadding) +
-  strlen(message);
-  for (n=0;n<messageLength;n++){
-    h = COLUMNS;
-    usleep(300000);
-    printf("\x1B[2J");//brisanje ekrana
-    if ( j > messageLength ) j = 0; 
-      for (i = 0; i < j; i ++){
-        scrollPadding[h-j] = message[ i];
-        h++;
-      }
-      lcdPosition(lcd,0,0);
-      lcdClear (lcd);
-      lcdPrintf(lcd,"%s",scrollPadding);
-      j++;
+    int lcd_h;
+    int x = 0;
+    int y = 0;
+    if (wiringPiSetup() < 0){
+      fprintf (stderr, "Greška pri inicijalizaciji: %s\n", strerror (errno)) ;
+      return 1 ;
     }
+    lcd_h = lcdInit(2, 16, 4, RS, EN, D0, D1, D2, D3, D0, D1, D2, D3);
+		
+    lcdPosition(lcd_h, x,y);
+    lcdCursor();
+    
+    struct lirc_config *config;
+		char *code;
+    
+    int i = 0;
+    
+		//startuj lirc
+		if(lirc_init("lirc",1)==-1)
+				return 1;
+     
+		//procitaj /etc/lirc/lirc/lircrc/lircd.conf
+		if(lirc_readconfig(NULL, &config,NULL)==0)
+		{ 
+				//radimo dok je LIRC soket otvoren 0=otvoren -1=zatvoren
+				while (lirc_nextcode(&code)==0)
+				{
+            
+						if(code==NULL) continue; {
+				
+								delay(400);
+						if (strstr(code,"KEY_NEXT")){
+              x += 1;
+            }else if (strstr(code,"KEY_PREVIOUS")){
+              x -= 1;
+            }else if (strstr(code,"KEY_UP")){
+              y += 1;
+            }else if (strstr(code,"KEY_DOWN")){
+              y -= 1;
+            }
+            
+            if(x == 16){
+                x = 0;
+            }else if(x == -1){
+                x = 15;
+            }
+             
+            if(y == 2){
+                y = 0;
+            }else if( y == -1){
+                y = 1;
+            }
+              
+            lcdPosition(lcd_h, x,y);
+              
+            
+						free(code);
+				}
+				lirc_freeconfig(config);
+		}
+		lirc_deinit();
+ 		return 0;
 }
