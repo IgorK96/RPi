89add1ead08c7f65b043c0cd3f4c9e24a6c5dc8f
IgorK96 <44954943+IgorK96@users.noreply.github.com>
2019-05-23 13:31:22 +0200

    Update dialog.cpp

--- V12/zad1/dialog.cpp
+++ V12/zad1/dialog.cpp
@@ -1,1 +1,42 @@
 #include "dialog.h"
+#include "ui_dialog.h"
+
+Dialog::Dialog(QWidget *parent) :
+	QMainWindow(parent),
+	ui(new Ui::Dialog)
+{
+	ui->setupUi(this);
+	temperature();
+
+}
+
+Dialog::~Dialog()
+{
+	delete ui;
+}
+
+Dialog::temperature()
+{
+	FILE *ft;
+	char tekst[100];
+	ft=fopen("/sys/bus/w1/devices/28-00000???????/w1_slave","r");
+
+	if(ft==NULL) return 0;
+
+	int i=0;
+
+	for(i=0;i<22;i++)//samo temperatura
+		fscanf(ft,"%s", tekst);
+
+	fclose(ft);
+
+	//obrisati „t=”
+	for(i=0;i<10;i++) tekst[i]=tekst[i+2];
+
+	int temp=atoi(tekst); //prebaci u double
+	double tem=(double)temp/1000;
+
+	return tem;
+	delay(1000);
+	ui->lcdNumber->display(tem);
+}
