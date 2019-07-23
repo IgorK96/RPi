b879b21a49a352b60f9ccfeec972a753c610787a
IgorK96 <44954943+IgorK96@users.noreply.github.com>
2019-07-23 22:49:58 +0200

    Update wPiRTC.c

--- wPiRTC.c
+++ wPiRTC.c
@@ -31,9 +31,11 @@ int hours, minutes, seconds;
 char txt_seconds[4];
 char txt_minutes[4];
 char txt_hours[4];
+
 int main()
-{
-	wiringPiSetup();
+{	
+	if(wiringPiSetup() == -1) exit(1);
+	//wiringPiSetup(); //Proveri dal radi !!! #1
 	pinMode (SENSOR, INPUT);
 	pinMode (BUTTON, INPUT);
 	pullUpDnControl(BUTTON, PUD_UP);
@@ -47,18 +49,17 @@ int main()
 	int dec_seconds;
 	int enable = 0;
 
-	if(wiringPiSetup() == -1) exit(1);
+	//if(wiringPiSetup() == -1) exit(1); //Proveri dal radi !!! #1
 
 	lcd_h = lcdInit(2, 16, 4, RS, EN, D0, D1, D2, D3, D0, D1, D2, D3);
-	lcdPosition(lcd_h, 0, 0);
-	lcdPrintf(lcd_h,"Vreme detekcije: ");
+	//lcdPrintf(lcd_h,"Vreme detekcije: ");
 
 	fd = wiringPiI2CSetup(RTC);
-	wiringPiI2CWriteReg8(fd, SEC, 0x00);
-	wiringPiI2CWriteReg8(fd, MIN, 0x59);
 	wiringPiI2CWriteReg8(fd, HOU, 0x25);
+	wiringPiI2CWriteReg8(fd, MIN, 0x59);
+	wiringPiI2CWriteReg8(fd, SEC, 0x00);
 
-	wiringPiSetup();
+	//wiringPiSetup();// Proveri dal radi !!! #1
 	while(1)
 	{	if(enable == 0){
 		sensor_value = digitalRead(SENSOR);
@@ -68,58 +69,62 @@ int main()
 			dec_hours = hours;
 			dec_minutes = minutes;
 			dec_seconds = seconds;
-		}else{
-		printf("Vrednost na senzoru: %d ",sensor_value);
-		hours = changeHexToInt(wiringPiI2CReadReg8(fd, HOU)& 0x3f); 
-		minutes = changeHexToInt(wiringPiI2CReadReg8(fd, MIN)& 0x7f);
-		seconds = changeHexToInt(wiringPiI2CReadReg8(fd, SEC)& 0x7f);
-
-		sprintf(txt_hours,"%d", hours);
-		sprintf(txt_minutes,"%d", minutes);
-		sprintf(txt_seconds,"%d", seconds);
-
-		printf("Vreme HH:MM:SS %d %d %d \n ", hours, minutes, seconds);
-		lcdPosition(lcd_h, 0, 0);
-		lcdPrintf(lcd_h,"No Movment");
-		lcdPosition(lcd_h, 0, 1);
-		lcdPrintf(lcd_h,"Time:");
-		lcdPosition(lcd_h, 6, 1);
-		lcdPrintf(lcd_h,txt_hours);
-		lcdPosition(lcd_h, 8, 1);
-		lcdPrintf(lcd_h,":");
-		lcdPosition(lcd_h, 9, 1);
-		lcdPrintf(lcd_h,txt_minutes);
-		lcdPosition(lcd_h, 11, 1);
-		lcdPrintf(lcd_h,":");
-		lcdPosition(lcd_h, 12, 1);
-		lcdPrintf(lcd_h,txt_seconds);
-		}
 		}else
-		{	sprintf(txt_hours, "%d", dec_hours);
-			sprintf(txt_minutes, "%d", dec_minutes);
-			sprintf(txt_seconds, "%d", dec_seconds);
-			lcdPosition(lcd_h, 0, 0);
-			lcdPrintf(lcd_h, "MOVMENT DETECTED !");
-			lcdPosition(lcd_h, 0, 1);
-			lcdPrintf(lcd_h, "Time: ");
-			lcdPosition(lcd_h, 6, 1);
-			lcdPrintf(lcd_h,txt_hours);
-			lcdPosition(lcd_h, 8, 1);
-			lcdPrintf(lcd_h,":");
-			lcdPosition(lcd_h, 9, 1);
-			lcdPrintf(lcd_h,txt_minutes);
-			lcdPosition(lcd_h, 11, 1);
-			lcdPrintf(lcd_h,":");
-			lcdPosition(lcd_h, 12, 1);
-			lcdPrintf(lcd_h,txt_seconds);
-			//printf("Movment detected at : %d %d %d", dec_hours, dec_minutes, dec_seconds);
-			button_value = digitalRead(BUTTON);
-			if(button_value == 0)
 			{
-				enable = 0;
-				lcdClear(lcd_h);
+				printf("Vrednost na senzoru: %d ",sensor_value);
+				hours = changeHexToInt(wiringPiI2CReadReg8(fd, HOU)& 0x3f);  //Treba ograniciti na 24 nekako ?
+				minutes = changeHexToInt(wiringPiI2CReadReg8(fd, MIN)& 0x7f);
+				seconds = changeHexToInt(wiringPiI2CReadReg8(fd, SEC)& 0x7f);
+
+				sprintf(txt_hours,"%d", hours);
+				sprintf(txt_minutes,"%d", minutes);
+				sprintf(txt_seconds,"%d", seconds);
+
+				//printf("Vreme HH:MM:SS %d %d %d \n ", hours, minutes, seconds);
+				lcdPosition(lcd_h, 0, 0);
+				lcdPrintf(lcd_h,"No Movment");
+				lcdPosition(lcd_h, 0, 1);
+				lcdPrintf(lcd_h,"Time:");
+				lcdPosition(lcd_h, 6, 1);
+				lcdPrintf(lcd_h,txt_hours);
+				lcdPosition(lcd_h, 8, 1);
+				lcdPrintf(lcd_h,":");
+				lcdPosition(lcd_h, 9, 1);
+				lcdPrintf(lcd_h,txt_minutes);
+				lcdPosition(lcd_h, 11, 1);
+				lcdPrintf(lcd_h,":");
+				lcdPosition(lcd_h, 12, 1);
+				lcdPrintf(lcd_h,txt_seconds);
 			}
-		}
+		}else
+			{	
+				sprintf(txt_hours, "%d", dec_hours);
+				sprintf(txt_minutes, "%d", dec_minutes);
+				sprintf(txt_seconds, "%d", dec_seconds);
+				
+				lcdPosition(lcd_h, 0, 0);
+				lcdPrintf(lcd_h, "MOVMENT DETECTED !");
+				lcdPosition(lcd_h, 0, 1);
+				lcdPrintf(lcd_h, "Time: ");
+				lcdPosition(lcd_h, 6, 1);
+				lcdPrintf(lcd_h,txt_hours);
+				lcdPosition(lcd_h, 8, 1);
+				lcdPrintf(lcd_h,":");
+				lcdPosition(lcd_h, 9, 1);
+				lcdPrintf(lcd_h,txt_minutes);
+				lcdPosition(lcd_h, 11, 1);
+				lcdPrintf(lcd_h,":");
+				lcdPosition(lcd_h, 12, 1);
+				lcdPrintf(lcd_h,txt_seconds);
+				//printf("Movment detected at : %d %d %d", dec_hours, dec_minutes, dec_seconds);
+				
+				button_value = digitalRead(BUTTON);
+				if(button_value == 0)
+				{
+					enable = 0;
+					lcdClear(lcd_h);
+				}
+			}
 	}
 	return 0;
 }
