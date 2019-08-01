c93ec3b29fce2cd41c4c9d0e7db35d4fcb4ca0ea
IgorK96 <44954943+IgorK96@users.noreply.github.com>
2019-08-01 23:43:42 +0200

    Update wPiRTC.c

--- wPiRTC.c
+++ wPiRTC.c
@@ -8,13 +8,15 @@
 #include <lcd.h>
 #include <string.h>
 
+//adrese registara
 #define SEC 0x02
 #define MIN 0x03
 #define HOU 0x04
 
 #define changeHexToInt(hex) ((((hex)>>4) *10 ) + ((hex)%16))
-const char RTC = 0x51;
+const char RTC = 0x51; //hardverska adresa RTC-a
 
+//PINOVI koje koristi LCD
 const int RS = 3;
 const int EN = 14;
 const int D0 = 4;
@@ -22,6 +24,7 @@ const int D1 = 12;
 const int D2 = 13;
 const int D3 = 6;
 
+//PIN na kome se nalazi senzor i dugme DVK512 plocici u wiringPi oznaci
 char SENSOR = 29;
 char BUTTON = 24;
 
@@ -34,10 +37,10 @@ char txt_hours[4];
 
 int main()
 {	
-	if(wiringPiSetup() == -1) exit(1);
-	pinMode (SENSOR, INPUT);
-	pinMode (BUTTON, INPUT);
-	pullUpDnControl(BUTTON, PUD_UP);
+	if(wiringPiSetup() == -1) exit(1); //inicijalizacija WiringPi
+	pinMode (SENSOR, INPUT); //PIN senzor je u INPUT modu
+	pinMode (BUTTON, INPUT); //PIN BUTTON je u INPUT modu
+	pullUpDnControl(BUTTON, PUD_UP); //dugme ce da radi u Pull up rezimu
 
 	int sensor_value = 0;
 	int button_value = 1;
@@ -49,57 +52,58 @@ int main()
 	int enable = 0;
 
 
-	lcd_h = lcdInit(2, 16, 4, RS, EN, D0, D1, D2, D3, D0, D1, D2, D3);
+	lcd_h = lcdInit(2, 16, 4, RS, EN, D0, D1, D2, D3, D0, D1, D2, D3); //Inicijalizacija LCD-a 2 - broj kolona,16-broj redova -4 bitni interfejs, pinovi koje koristi lcd u 4bitnom interfejsu
 
-	fd = wiringPiI2CSetup(RTC);
-	wiringPiI2CWriteReg8(fd, HOU, 0x23);
+	fd = wiringPiI2CSetup(RTC); //inicijalizacija RTC i postavljanje pocetnih vrednosti u Registre za SATE MINUTE i SEKUNDE
+	wiringPiI2CWriteReg8(fd, HOU, 0x23); 
 	wiringPiI2CWriteReg8(fd, MIN, 0x59);
 	wiringPiI2CWriteReg8(fd, SEC, 0x00);
 
 	while(1)
-	{	if(enable == 0){
-		sensor_value = digitalRead(SENSOR);
-		if(sensor_value == 1)
+	{	if(enable == 0)
 		{
-			enable = 1;
-			detc_hours = hours;
-			detc_minutes = minutes;
-			detc_seconds = seconds;
-		}else
+			sensor_value = digitalRead(SENSOR); //citamo vrednost sa senzora 1 je ako je detektovao nesto , 0 ako nije 
+			if(sensor_value == 1)
 			{
-				//printf("Vrednost na senzoru: %d ",sensor_value);
-				hours = changeHexToInt(wiringPiI2CReadReg8(fd, HOU)& 0x3f);  //Treba ograniciti na 24 nekako ?
-				minutes = changeHexToInt(wiringPiI2CReadReg8(fd, MIN)& 0x7f);
-				seconds = changeHexToInt(wiringPiI2CReadReg8(fd, SEC)& 0x7f);
+				enable = 1;
+				detc_hours = hours;
+				detc_minutes = minutes;
+				detc_seconds = seconds;
+			}else
+				{
+					printf("Vrednost na senzoru: %d ",sensor_value);
+					hours = changeHexToInt(wiringPiI2CReadReg8(fd, HOU)& 0x3f);   // Citamo vreme iz registara za sata, prebacujemo u int i smestamo u hours , maska 0x3f jer nekoristi 7 i 6 bit pa 0011 1111 je 0x3f u hexu
+					minutes = changeHexToInt(wiringPiI2CReadReg8(fd, MIN)& 0x7f); //isto ko i gore samo druga maska za MIN
+					seconds = changeHexToInt(wiringPiI2CReadReg8(fd, SEC)& 0x7f);
 
-				sprintf(txt_hours,"%d", hours);
-				sprintf(txt_minutes,"%d", minutes);
-				sprintf(txt_seconds,"%d", seconds);
+					sprintf(txt_hours,"%d", hours);  //prebacujemo integere u stringove 
+					sprintf(txt_minutes,"%d", minutes);
+					sprintf(txt_seconds,"%d", seconds);
 
-				//printf("Vreme HH:MM:SS %d %d %d \n ", hours, minutes, seconds);
-				lcdPosition(lcd_h, 0, 0);
-				lcdPrintf(lcd_h,"No Movment");
-				lcdPosition(lcd_h, 0, 1);
-				lcdPrintf(lcd_h,"Time:");
-				lcdPosition(lcd_h, 6, 1);
-				lcdPrintf(lcd_h,txt_hours);
-				lcdPosition(lcd_h, 8, 1);
-				lcdPrintf(lcd_h,":");
-				lcdPosition(lcd_h, 9, 1);
-				lcdPrintf(lcd_h,txt_minutes);
-				lcdPosition(lcd_h, 11, 1);
-				lcdPrintf(lcd_h,":");
-				lcdPosition(lcd_h, 12, 1);
-				lcdPrintf(lcd_h,txt_seconds);
-			}
+					printf("Vreme HH:MM:SS %d %d %d \n ", hours, minutes, seconds);
+					lcdPosition(lcd_h, 0, 0); //namestamo kursor LCDa na 0,0 (x,y) kordinate
+					lcdPrintf(lcd_h,"No Movment"); //ispisujemo na toj poziciji dati string
+					lcdPosition(lcd_h, 0, 1);
+					lcdPrintf(lcd_h,"Time:");
+					lcdPosition(lcd_h, 6, 1);
+					lcdPrintf(lcd_h,txt_hours);
+					lcdPosition(lcd_h, 8, 1);
+					lcdPrintf(lcd_h,":");
+					lcdPosition(lcd_h, 9, 1);
+					lcdPrintf(lcd_h,txt_minutes);
+					lcdPosition(lcd_h, 11, 1);
+					lcdPrintf(lcd_h,":");
+					lcdPosition(lcd_h, 12, 1);
+					lcdPrintf(lcd_h,txt_seconds);
+				}
 		}else
 			{	
-				sprintf(txt_hours, "%d", detc_hours);
+				sprintf(txt_hours, "%d", detc_hours); 
 				sprintf(txt_minutes, "%d", detc_minutes);
 				sprintf(txt_seconds, "%d", detc_seconds);
-				
-				lcdPosition(lcd_h, 0, 0);
-				lcdPrintf(lcd_h, "MOVMENT DETECTED !");
+				//mozda stavi ovde jos jedan lcdClear(lcd_h) 
+				lcdPosition(lcd_h, 0, 0);  
+				lcdPrintf(lcd_h, "MOVMENT DETECTED !"); 
 				lcdPosition(lcd_h, 0, 1);
 				lcdPrintf(lcd_h, "Time: ");
 				lcdPosition(lcd_h, 6, 1);
@@ -112,13 +116,13 @@ int main()
 				lcdPrintf(lcd_h,":");
 				lcdPosition(lcd_h, 12, 1);
 				lcdPrintf(lcd_h,txt_seconds);
-				//printf("Movment detected at : %d %d %d", dec_hours, dec_minutes, dec_seconds);
+				printf("Movment detected at : %d %d %d \n", dec_hours, dec_minutes, dec_seconds);
 				
-				button_value = digitalRead(BUTTON);
-				if(button_value == 0)
+				button_value = digitalRead(BUTTON);  //citamo vrednost sa dugmeta (ili 1 ili 0)
+				if(button_value == 0)  //ako je dugme stisnuto
 				{
 					enable = 0;
-					lcdClear(lcd_h);
+					lcdClear(lcd_h); //brisemo sadrzaj sa LCD-a
 				}
 			}
 	}
